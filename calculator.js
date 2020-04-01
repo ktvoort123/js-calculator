a7d65a872fd55e939abac8c02aaba914e99011e7
Katie <katie.vandervoort@gmail.com>
2020-03-31 18:33:49 -0700

    cleaned up code

--- calculator.js
+++ calculator.js
@@ -1,10 +1,3 @@
-// const exampleAdditionInput = {
-//   num1: 3,
-//   num2: 5,
-//   operation: 'add',
-// }
-
-
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1,num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
@@ -15,27 +8,21 @@ const calculate = function(input) {
   num2 = input.num2;
   operation = input.operation.toLowerCase();
 
-  if(isNaN(num1) || isNaN(num2)) {
-    return "one or both of the numbers are not valid"
-  }
+  if(isNaN(num1) || isNaN(num2)) return "one or both of the numbers are not valid";
   
   if(operation === "add" || operation === "+") {
-    return add(num1, num2)
+    return add(num1, num2);
   } else if(operation === "subtract" || operation === "-" || operation === "minus") {
-    return subtract(num1, num2)
+    return subtract(num1, num2);
   } else if(operation === "multiply" || operation === "*") {
-    return multiply(num1, num2)
+    return multiply(num1, num2);
   } else if(operation === "divide" || operation === "/") {
-    if(num2 === 0) {
-      return "Can't divide by 0!" 
-    } else {
-      return divide(num1, num2)
-    }
+    if(num2 === 0) return "Can't divide by 0!";
+    return divide(num1, num2);
   } else {
     return "not a valid operator"
   }
 }
-
 
 const testCases = [{
   num1: 3,
@@ -97,8 +84,6 @@ const testCases = [{
   num2: 'dog',
   operation: 'divide',
 }
-]
+];
 
-
-testCases.forEach(testCase => console.log(`${testCase.num1} ${testCase.operation} ${testCase.num2} = ${calculate(testCase)}`))
-
+testCases.forEach(testCase => console.log(`${testCase.num1} ${testCase.operation} ${testCase.num2} = ${calculate(testCase)}`));
